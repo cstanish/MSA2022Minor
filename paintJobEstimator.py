77df9cb9fedf8a199c900ec31a6ac7d4c3601499
Kristofferson Culmer <krcd58@mail.missouri.edu>
2022-06-16 14:42:44 -0500

    Add files via upload

--- paintJobEstimator.py
+++ paintJobEstimator.py
@@ -4,13 +4,24 @@ hourly_labor_cost = 62.25
 unit_of_wall_area = 350
 hours_labor_per_unit = 6
 #prompt the user to enter the amount of wall to paint
-wall_area = input("What is the area of wall in sq/ft: ")
+#Convert to float
+#if error in input ask user to re-enter input. Input must be greater than 0
+run_again = True
+while (run_again):
+    try:
+        wall_area = float(input("What is the area of wall in sq/ft: "))
+        if(wall_area <= 0):
+            continue
+    except:
+        print("ERROR: Wall area must be a number.\n")
+    else:
+        run_again = False
 #prompt user to enter cost of paint per gallon
-paint_price = input("What is the price of paint per gallon: ")
-test_number = wall_area / paint_price
+#Convert to float
+paint_price = float(input("What is the price of paint per gallon: "))
 
 #PROCESS
-#Calculate to hours of labor
+#Calculate to hours of laborjhfdjhf
 #Calculate the cost of labor
 #Calculate the amount of paint
 #Calculate the cost of the paint
